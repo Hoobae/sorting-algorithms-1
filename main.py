0515f1619ba4fc0f0bf351db8a4c6e8d9f3f8337
JulioGQ <jgq.cd24@gmail.com>
2022-05-19 13:43:13 -0500

    Bubble Sort Added

--- main.py
+++ main.py
@@ -1,16 +1,64 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+k1 = []
+for x in range(1000):
+    k1.append(random.randint(1, 1000))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+k2 = []
+for y in range(2000):
+    k2.append(random.randint(1, 2000))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+k3 = []
+for z in range(3000):
+    k3.append(random.randint(1, 3000))
+
+
+def bubbleSort(arr):
+    n = len(arr)
+
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+print("k1 desordenado:")
+for i in range(len(k1)):
+    print("% d" % k1[i], end=" ")
+
+bubbleSort(k1)
+
+print("\nk1 ordenado:")
+for i in range(len(k1)):
+    print("% d" % k1[i], end=" ")
+
+print("\nk2 desordenado:")
+for i in range(len(k2)):
+    print("% d" % k2[i], end=" ")
+
+bubbleSort(k2)
+
+print("\nk2 ordenado:")
+for i in range(len(k2)):
+    print("% d" % k2[i], end=" ")
+
+print("\nk3 desordenado:")
+for i in range(len(k3)):
+    print("% d" % k3[i], end=" ")
+
+bubbleSort(k3)
+
+print("\nk3 ordenado:")
+for i in range(len(k3)):
+    print("% d" % k3[i], end=" ")
+
+
+k1 = []
+print("\nk1 vacio:")
+for i in range(len(k1)):
+    print("% d" % k1[i], end=" ")
+
+
+
